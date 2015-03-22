4697ce6832f45c0bb356117aaa0ad6b12f837d2e
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-03-21 23:25:57 -0700

    spider for health sales only

---
+++ kcrawler/kcrawler/spiders/bzhealthSpider.py
@@ -0,0 +1,44 @@
+import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.selector import Selector
+# from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.linkextractors.lxmlhtml import LxmlLinkExtractor
+from kcrawler.items import KcrawlerItem
+
+class bzhealthSpider(CrawlSpider):
+    name = 'bzhealthSpider'
+    allowed_domains = ['bizbuysell.com']
+    start_urls = ['http://www.bizbuysell.com/health-care-companies-for-sale/']
+
+    # rules = (
+    #     # Extract links matching 'category.php' (but not matching 'subsection.php')
+    #     # and follow links from them (since no callback means follow=True by default).
+    #     Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
+
+    #     # Extract links matching 'item.php' and parse them with the spider's method parse_item
+    #     Rule(LinkExtractor(allow=('item\.php', )), callback='parse_item'),
+    # )
+
+    rules = (
+        Rule(LxmlLinkExtractor(allow=('/health-care-companies-for-sale'),restrict_xpaths=('//div[@class="pagination"]/ul/li/a[@title="Next Page"]')),callback='parse_items',follow=True),
+        # Rule(LxmlLinkExtractor(allow=('/health-care-companies-for-sale')),callback='parse_item'),
+    )
+
+    def parse_items(self, response):
+        sel = Selector(response)
+        lists = sel.xpath('//a[contains(@id,"List") and not(contains(@id,"ListNumber"))]')
+        items = []
+
+        for li in lists:
+            item = KcrawlerItem()
+            item['source'] = u'bizbuysell'
+            item['title'] = li.xpath('span[2]/b[@class="title"]/text()').extract()
+            item['link'] = li.xpath('@href').extract()
+            item['location'] = li.xpath('span[2]/p[@class="info"]/b/text()').extract()
+            item['desc'] = li.xpath('span[2]/p[@class="desc"]/text()').extract()
+            # item['url'] = li.xpath('a/@href').extract()
+            # item['description'] = li.xpath('text()').re('-\s[^\n]*\\r')
+            items.append(item)
+            print item ['title']
+
+        return items
