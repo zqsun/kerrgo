050d567a7ec6c6a2cd81f2f5d462f8f9ec9421e1
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-03-24 00:28:16 -0700

    implement mysql pipeline

--- kcrawler/kcrawler/pipelines.py
+++ kcrawler/kcrawler/pipelines.py
@@ -6,6 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from scrapy.exceptions import DropItem
+import MySQLdb
 
 class KcrawlerPipeline(object):
     def process_item(self, item, spider):
@@ -22,3 +23,25 @@ class DuplicatesPipeline(object):
         else:
             self.ids_seen.add(item['id'])
             return item
+
+class MySQLStorePipeline(object):
+  	def __init__(self):
+    	self.conn = MySQLdb.connect(user='kerrgo', '123456', 'kerrgo', '127.0.0.1', charset="utf8", use_unicode=True)
+    	self.cursor = self.conn.cursor()
+
+	def process_item(self, item, spider):    
+    	try:
+        	self.cursor.execute("""INSERT INTO kerrgo_test (title, link,location,description,source,category)  
+            	            VALUES (%s, %s, %s, %s, %s, %s)""", 
+                	       (item['title'].encode('utf-8'), 
+                    	    item['link'].encode('utf-8'),
+                        	item['location'].encode('utf-8'),
+                        	item['desc'].encode('utf-8'), 
+                        	item['source'].encode('utf-8'),
+                        	item['category'].encode('utf-8'),
+                        	)
+                       	)
+        	self.conn.commit()
+    	except MySQLdb.Error, e:
+        	print "Error %d: %s" % (e.args[0], e.args[1])
+    	return item
