c52561672041d0743b6cb7f2d1926448c2f035e6
Zhongqi Sun <zhongqi.sun@gmail.com>
2015-03-30 15:28:09 -0700

    Modified the sql pipeline and now it can insert data to the mysql db

--- kcrawler/kcrawler/pipelines.py
+++ kcrawler/kcrawler/pipelines.py
@@ -12,36 +12,46 @@ class KcrawlerPipeline(object):
     def process_item(self, item, spider):
         return item
 
-class DuplicatesPipeline(object):
+# class DuplicatesPipeline(object):
 
-    def __init__(self):
-        self.ids_seen = set()
+#     def __init__(self):
+#         self.ids_seen = set()
 
-    def process_item(self, item, spider):
-        if item['id'] in self.ids_seen:
-            raise DropItem("Duplicate item found: %s" % item)
-        else:
-            self.ids_seen.add(item['id'])
-            return item
+#     def process_item(self, item, spider):
+#         if item['link'] in self.ids_seen:
+#             raise DropItem("Duplicate item found: %s" % item)
+#         else:
+#             self.ids_seen.add(item['id'])
+#             return item
 
 class MySQLStorePipeline(object):
-  	def __init__(self):
-    	self.conn = MySQLdb.connect(user='kerrgo', '123456', 'kerrgo', '127.0.0.1', charset="utf8", use_unicode=True)
-    	self.cursor = self.conn.cursor()
 
-	def process_item(self, item, spider):    
-    	try:
-        	self.cursor.execute("""INSERT INTO kerrgo_test (title, link,location,description,source,category)  
-            	            VALUES (%s, %s, %s, %s, %s, %s)""", 
-                	       (item['title'].encode('utf-8'), 
-                    	    item['link'].encode('utf-8'),
-                        	item['location'].encode('utf-8'),
-                        	item['desc'].encode('utf-8'), 
-                        	item['source'].encode('utf-8'),
-                        	item['category'].encode('utf-8'),
-                        	)
-                       	)
-        	self.conn.commit()
-    	except MySQLdb.Error, e:
-        	print "Error %d: %s" % (e.args[0], e.args[1])
-    	return item
+  	def __init__(self):
+  		self.conn = MySQLdb.connect(host='localhost',user='kerrgo',passwd='123456',db='kerrgo')
+  		self.cursor = self.conn.cursor()
+  	def process_item(self,item,spider):
+  		try:
+  			# self.cursor.execute("""INSERT INTO kerrgo_test (title,link,location,description,source,category) VALUES (%s,%s,%s,%s,%s,%s)""",
+  			# 	(item['title'][0],
+  			# 	item['location'][0],
+  			# 	item['desc'][0],
+  			# 	item['source'],
+  			# 	item['category']))
+			self.cursor.execute("""INSERT INTO kerrgo_test (title,link,location,description,source,category) VALUES (%s,%s,%s,%s,%s,%s)""",
+  				(item['title'],item['link'],item['location'],item['desc'],item['source'],item['category']))
+  			self.conn.commit()
+  		except MySQLdb.Error, e:
+  			print "Error %d: %s" %(e.args[0],e.args[1])
+  		return item
+  		# try:
+    # 		self.cursor.execute("INSERT INTO kerrgo_test (title, link,location,description,source,category) VALUES (%s,%s,%s,%s,%s,%s)",
+    # 			(item['title'].encode('utf-8'),
+    # 				item['link'].encode('utf-8'),
+    # 				item['location'].encode('utf-8'),
+    # 				item['desc'].encode('utf-8'),
+    # 				item['source'].encode('utf-8'),
+    # 				item['category'].encode('utf-8')))
+    # 	except MySQLdb.Error, e:
+    # 		print "Error %d: %s" %(e.args[0],e.args[1])
+    # 	return item
+    
